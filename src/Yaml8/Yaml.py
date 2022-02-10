a87f4f084b86dc3d51e2437bad1fd6e6c44840b1
AppDevIn <teamprojectlive@gmail.com>
2022-02-10 19:46:35 +0800

    Updated all the files 🔥

--- src/Yaml8/Yaml.py
+++ src/Yaml8/Yaml.py
@@ -1,13 +1,21 @@
-import pdb
-
 import yaml
+
+
+def read_yaml(path) -> {}:
+    config = {}
+    with open(path) as file:
+        try:
+            config = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
 
 
 class Yaml:
 
     def __init__(self, path):
         super(Yaml, self).__init__()
-        self.config = self.readYaml(path)
+        self.config = read_yaml(path)
 
     def value(self, value: str):
         def fun(f):
@@ -18,12 +26,3 @@ class Yaml:
             return r_value
 
         return fun
-
-    def readYaml(self, path) -> {}:
-        config = {}
-        with open(path) as file:
-            try:
-                config = yaml.safe_load(file)
-            except yaml.YAMLError as exc:
-                print(exc)
-        return config
