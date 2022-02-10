c1140c1954e0ea233cd32ad9faa8fc2051dd5369
AppDevIn <teamprojectlive@gmail.com>
2022-02-10 19:40:21 +0800

    Updated the configuration files 🔧🔧

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(
-   name='Yaml-Python',
-   version='1.0',
-   author='Sliver',
-   author_email='jeyavishnu@yahoo.com',
-   py_modules=['yaml']
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="Yaml8",
+    version="0.1.0",
+    author="AppDevIn Sliver",
+    author_email=" teamprojectlive@example.com",
+    description="Python package to make getting yaml easier and cleaner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AppDevIn/Yaml-Python",
+    project_urls={
+        "Bug Tracker": "https://github.com/AppDevIn/Yaml-Python/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+      "PyYAML==6.0"
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
 )
